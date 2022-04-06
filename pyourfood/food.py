1052732005f9a1e738618fd875dee09d6b4ed9ef
Juan <juan.cordero@postgrad.manchester.ac.uk>
2022-04-06 17:17:49 +0100

    Utility functions

--- pyourfood/food.py
+++ pyourfood/food.py
@@ -1,42 +1,69 @@
 import numpy as np
 import pandas as pd
-from .food_data import FAOSTAT
-
-FAOSTAT_years = FAOSTAT.years
-FAOSTAT_item_codes = FAOSTAT.item_codes
-FAOSTAT_item_names = FAOSTAT.item_names
-FAOSTAT_data = FAOSTAT.data
-
-def _tolist(item):
-    item = np.array(item).tolist()
-    if not isinstance(item, list):
-        item = [item]
-    return item
+from .utils.list_tools import tolist
 
 def extract_item(in_data, item):
-    item = _tolist(item)
+    item = tolist(item)
     out_data = in_data[in_data['Item Code'].isin(item)]
     return out_data
 
 def extract_year(in_data, year):
-    year = _tolist(year)
+    year = tolist(year)
     out_data = in_data[in_data['Year'].isin(year)]
     return out_data
 
-def food_supply(in_data, item=FAOSTAT_item_codes, year = FAOSTAT_years):
+def food_supply(in_data, item=None, year=None):
+
+    if item is None:
+        item = np.unique(in_data['Item Code'])
+
+    if year is None:
+        year = np.unique(in_data['Year'])
+
+    out_data = in_data[in_data['Element Code'] == 10004]
+    out_data = extract_year(out_data, year)
+    out_data = extract_item(out_data, item)
+
+    return out_data['Value']
+
+def energy_supply(in_data, item=None, year=None):
+
+    if item is None:
+        item = np.unique(in_data['Item Code'])
+
+    if year is None:
+        year = np.unique(in_data['Year'])
+
     out_data = in_data[in_data['Element Code'] == 664]
     out_data = extract_year(out_data, year)
     out_data = extract_item(out_data, item)
-    return out_data
 
-def protein_supply_quantity(in_data, item=FAOSTAT_item_codes, year = FAOSTAT_years):
+    return out_data['Value']
+
+def protein_supply_quantity(in_data, item=None, year = None):
+
+    if item is None:
+        item = np.unique(in_data['Item Code'])
+
+    if year is None:
+        year = np.unique(in_data['Year'])
+
     out_data = extract_item(in_data, item)
     out_data = extract_year(out_data, year)
     out_data = out_data[out_data['Element Code'] == 674]
-    return out_data
 
-def fat_supply_quantity(in_data, item=FAOSTAT_item_codes, year = FAOSTAT_years):
+    return out_data['Value']
+
+def fat_supply_quantity(in_data, item=None, year = None):
+
+    if item is None:
+        item = np.unique(in_data['Item Code'])
+
+    if year is None:
+        year = np.unique(in_data['Year'])
+
     out_data = extract_item(in_data, item = item)
     out_data = extract_year(out_data, year = year)
     out_data = out_data[out_data['Element Code'] == 684]
-    return out_data
+
+    return out_data['Value']
--- pyourfood/population_data/__init__.py
+++ pyourfood/population_data/__init__.py
@@ -1,12 +1,15 @@
 import os
 import numpy as np
 
-class UN_population:
-    UN_estimation_file = os.path.join(os.path.dirname(__file__), 'data/Total_population_UN_median_world_projected_2020_2100.txt')
+class UN_estimation:
+    UN_estimation_file = os.path.join(os.path.dirname(__file__), 'data/Total_population_UN_median_world.txt')
+
+    population = np.loadtxt(UN_estimation_file)
+    years = np.arange(1961, 2020)
+
+class UN_projection:
+
     UN_projection_file = os.path.join(os.path.dirname(__file__), 'data/Total_population_UN_median_world_projected_2020_2100.txt')
 
-    estimation = np.loadtxt(UN_estimation_file)
-    projection = np.loadtxt(UN_projection_file)
-
-    years_estimation = np.arange(1961, 2020)
-    years_projection = np.arange(2020, 2100)
+    population = np.loadtxt(UN_projection_file)
+    years = np.arange(2020, 2101)
