873969632bc4cd8c5900f31f08867a506e008642
Juan <juan.cordero@postgrad.manchester.ac.uk>
2022-08-25 12:37:02 +0100

    pixelised CEH cropmaps, ALC grades and base land methods

---
+++ agrifoodpy/land/land.py
@@ -0,0 +1,20 @@
+import numpy as np
+import xarray as xr
+import os
+from itertools import product
+
+data_dir = os.path.join(os.path.dirname(__file__), 'data/' )
+
+resolutions = [100, 200, 500, 1000, 2000]
+datasets = ["CEH", "ALC"]
+arrays = [f"{d}_{r}" for d, r in product(datasets, resolutions)] 
+
+def __getattr__(name):
+    if name not in arrays:
+        raise AttributeError(f"{name!r} does not match a dataset and resoliution in {__name__!r}.")
+
+    dataset, resolution = name.split('_')
+    _data_file = f'{data_dir}{dataset}/{name}.nc'
+    data = xr.open_dataset(_data_file)
+
+    return data
