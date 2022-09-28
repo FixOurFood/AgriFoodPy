6a015ea46e0cb2373216cb91ff021edbe9c9e599
Juan <juan.cordero@postgrad.manchester.ac.uk>
2022-09-28 17:09:51 +0100

    Support for multi-region-year plotting and scaling

--- agrifoodpy/food/food_supply.py
+++ agrifoodpy/food/food_supply.py
@@ -4,13 +4,27 @@ import os
 import copy
 
 import matplotlib.pyplot as plt
-import collections
 import warnings
 
 from ..utils.list_tools import tolist
 
 data_dir = os.path.join(os.path.dirname(__file__), 'data/' )
 available = ['FAOSTAT']
+
+FAOSTAT_elements = ['production',
+                    'imports',
+                    'exports',
+                    'stock',
+                    'feed',
+                    'seed',
+                    'losses',
+                    'processing',
+                    'residual',
+                    'tourist',
+                    'domestic',
+                    'other',
+                    'food',
+                    ]
 
 def FoodSupply(items, years, quantities, regions=None, elements=None):
 
@@ -46,8 +60,23 @@ def FoodSupply(items, years, quantities, regions=None, elements=None):
 
 def scale_food(food, scale, origin, items=None, constant=False, fallback=None):
 
+    if np.isscalar(items):
+        items = [items]
+
+    input_item_list = food.Item.values.tolist()
+
+    if np.isscalar(input_item_list):
+        input_item_list = [input_item_list]
+        sel = {}
+        non_sel = {}
+        if constant:
+            warnings.warn("Constant set to true but input only has one item.")
+            constant = False
+    else:
+        sel = {"Item":items}
+
     # If no items are provided, we scale all of them.
-    if items is None or np.sort(items) is np.sort(food.Item.values):
+    if items is None or np.sort(items) is np.sort(input_item_list):
         items = food.Item.values
         if constant:
             warnings.warn("Cannot keep food constant when scaling all items.")
@@ -61,95 +90,148 @@ def scale_food(food, scale, origin, items=None, constant=False, fallback=None):
     out = copy.deepcopy(food)
 
     # Scale items
-    out["food"].loc[items] *= scale
+    out["food"].loc[sel] *= scale
 
     # Compute differente needed to be added to origin.
     # If origin is exports, then set it to be negative.
-    delta = out["food"].loc[items] - food["food"].loc[items]
+    delta = out["food"].loc[sel] - food["food"].loc[sel]
 
     if origin == "exports":
-        out[origin].loc[items] -= delta
+        out[origin].loc[sel] -= delta
     else:
-        out[origin].loc[items] += delta
+        out[origin].loc[sel] += delta
 
     # If constant, total food is kept constant, at the expense of non-selected items
     if constant:
+
         nonsel = out.drop_sel(Item=items)
         nonsel_items = nonsel.Item.values
+        non_sel = {"Item":nonsel_items}
 
         # Compute scale for non-selected items
-        nonsel_scale = (nonsel["food"].sum() - delta.sum()) / nonsel["food"].sum()
-        if nonsel_scale < 0:
-            print(nonsel_scale)
+        nonsel_scale = (nonsel["food"].sum(dim="Item") - delta.sum(dim="Item")) / nonsel["food"].sum(dim="Item")
+        if np.any(nonsel_scale < 0):
             warnings.warn("Additional consumption cannot be compensated by reduction of non-selected items")
 
-        out["food"].loc[nonsel_items] *= nonsel_scale
-        nonsel_delta = out["food"].loc[nonsel_items] - food["food"].loc[nonsel_items]
-        out[origin].loc[nonsel_items] += nonsel_delta
+        out["food"].loc[non_sel] *= nonsel_scale
+        nonsel_delta = out["food"].loc[non_sel] - food["food"].loc[non_sel]
+        out[origin].loc[non_sel] += nonsel_delta
+
+        delta_fallback = out[origin].where(out[origin] < 0).fillna(0)
 
         if fallback == "exports":
-            out[fallback].loc[out[origin] < 0] -= out[origin].loc[out[origin] < 0]
+            out[fallback] -= delta_fallback
         else:
-            out[fallback].loc[out[origin] < 0] += out[origin].loc[out[origin] < 0]
+            out[fallback] += delta_fallback
 
         out[origin] = out[origin].where(out[origin] > 0, 0)
 
     return out
 
-def plot_food(food, items=None, colors=None, sharex=False):
+def plot_bars(food, show="Item", ax=None, colors=None, **kwargs):
 
-    # Check if a list of food xarrays was provided. If single one, convert to list
-    to_plot = food
-    if not isinstance(food, collections.abc.Iterable):
-        to_plot = (food, )
+    if ax is None:
+        f, ax = plt.subplots(**kwargs)
 
-    fig, axs = plt.subplots(len(to_plot), figsize=(10,3*len(to_plot)), sharex=sharex)
-    plt.subplots_adjust(hspace=0)
+    # Make sure only FAOSTAT food elements are present
+    input_elements = list(food.keys())
+    plot_elements = ["production", "imports", "exports", "food"]
 
-    for ax, unit in zip(axs, to_plot):
+    for element in plot_elements:
+        if element not in input_elements:
+            elements.remove(element)
 
-        # If not list of items is provided, plot all the items
-        if items is None:
-            items = unit.Item.values
+    for element in input_elements:
+        if element not in plot_elements and element in FAOSTAT_elements:
+            plot_elements.insert(-1, element)
 
-        # If colors are not defined, generate a list from the standard cycling
-        if colors is None:
-            colors = [f"C{ic}" for ic in range(len(items))]
+    len_elements = len(plot_elements)
 
-        elements = list(unit.keys())
+    # Define dimensions to sum over
+    bar_dims = list(food.dims)
+    if show in bar_dims:
+        bar_dims.remove(show)
+        size_show = food.sizes[show]
+    else:
+        size_show = 1
 
-        # Plot the production and imports first
-        cumul = 0
-        for ie, element in enumerate(["production", "imports"]):
-            for ii, item in enumerate(items):
-                val = np.sum(unit[element].sel(Item=item))
+    food_sum = food.sum(dim=bar_dims)
+
+    # If colors are not defined, generate a list from the standard cycling
+    if colors is None:
+        colors = [f"C{ic}" for ic in range(size_show)]
+
+    # Plot the production and imports first
+    cumul = 0
+    for ie, element in enumerate(["production", "imports"]):
+        if size_show == 1:
+            ax.barh(ie, left = cumul, width=food_sum[element], color=colors[0])
+            cumul +=food_sum[element]
+        else:
+            for ii, val in enumerate(food_sum[element]):
                 ax.barh(ie, left = cumul, width=val, color=colors[ii])
                 cumul += val
 
-        subs = copy.deepcopy(elements)
-        subs.remove("imports")
-        subs.remove("production")
-
-        # Then the rest of items
-        cumul = 0
-        for ie, element in enumerate(reversed(subs)):
-            for ii, item in enumerate(items):
-                val = np.sum(unit[element].sel(Item=item))
-                ax.barh(9-ie, left = cumul, width=val, color=colors[ii])
+    # Then the rest of elements in reverse to keep dimension ordering
+    cumul = 0
+    for ie, element in enumerate(reversed(plot_elements[2:])):
+        if size_show == 1:
+            ax.barh(len_elements-1 - ie, left = cumul, width=food_sum[element], color=colors[0])
+            cumul +=food_sum[element]
+        else:
+            for ii, val in enumerate(food_sum[element]):
+                ax.barh(len_elements-1 - ie, left = cumul, width=val, color=colors[ii])
                 cumul += val
 
-        ax.set_yticks(np.arange(len(elements)))
-        ax.set_xticks(ax.get_xticks()[1:-1])
-        ax.tick_params(axis="x",direction="in", pad=-12)
-        ax.set_yticklabels(labels=elements)
-        ax.invert_yaxis()  # labels read top-to-bottom
-        ax.set_ylim(len(elements)+1,-1)
+    # Plot decorations
+    ax.set_yticks(np.arange(len_elements), labels=plot_elements)
+    ax.set_yticks(ax.get_yticks())
+    ax.tick_params(axis="x",direction="in", pad=-12)
+    ax.invert_yaxis()  # labels read top-to-bottom
+    ax.set_ylim(len_elements+1,-1)
 
-    return fig, axs
+    return ax
+
+def plot_years(food, show="Item", ax=None, colors=None, **kwargs):
+
+    # If no years are found in the dimensions, raise an exception
+    sum_dims = list(food.dims)
+    if "Year" not in sum_dims:
+        raise TypeError("'Year' dimension not found in array data")
+
+    # Define the cumsum and sum dimentions and check for one element dimensions
+    sum_dims.remove("Year")
+    if ax is None:
+        f, ax = plt.subplots(1, **kwargs)
+
+    if show in sum_dims:
+        sum_dims.remove(show)
+        size_cumsum = food.sizes[show]
+        cumsum = food.cumsum(dim=show).transpose(show, ...)
+    else:
+        size_cumsum = 1
+        cumsum = food
+
+    # Collapse remaining dimensions
+    cumsum = cumsum.sum(dim=sum_dims)
+    years = food.Year.values
+
+    # If colors are not defined, generate a list from the standard cycling
+    if colors is None:
+        colors = [f"C{ic}" for ic in range(size_cumsum)]
+
+    # Plot
+    if size_cumsum == 1:
+        ax.fill_between(years, cumsum, alpha=0.5)
+        ax.plot(years, cumsum, color=colors[0], linewidth=0.5)
+    else:
+        for id in reversed(range(size_cumsum)):
+            ax.fill_between(years, cumsum[id], alpha=0.5)
+            ax.plot(years, cumsum[id], color=colors[id], linewidth=0.5)
+
+    return ax
 
 def SSR(food, items=None, per_item=False):
-
-    # Should per_year and per_region be also added?
 
     if items is not None:
         food = food.sel(Item=items)
