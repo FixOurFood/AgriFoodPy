22e4881923c5a617e63dbbb06ad5b364d46a706d
Juan Pablo Cordero <34517350+jucordero@users.noreply.github.com>
2023-03-15 12:33:38 +0000

    impact and population constructors (#31)
    I'll merge this to move on. The constructors work for the few plausible cases I see ir being used.
    If we decide to change this down the road we can open a new issue + PR.
    I'll close #30 for now. 
    
    * impact and population constructors
    * Added fix to food supply constructors, limit cases

--- agrifoodpy/impact/impact.py
+++ agrifoodpy/impact/impact.py
@@ -5,28 +5,87 @@ import os
 data_dir = os.path.join(os.path.dirname(__file__), 'data/' )
 available = ['PN18', 'PN18_FAOSTAT']
 
-def impacts(items, impacts, quantities, regions=None):
-    _impacts = np.unique(impacts)
+def impacts(items, regions, quantities, datasets=None, long_format=True):
+    """Impact style dataset constructor
+
+    Parameters
+    ----------
+    items : (ny,) array_like
+        The Item identifying ID or strings for which coordinates will be
+        created.
+    regions : (nr,) array_like, optional
+        The region identifying ID or strings for which coordinates will be
+        created.
+    quantities : (ny, nr) ndarray
+        Array containing the quantities for each combination of `Item` and
+        `Region`.
+    datasets : (nd,) array_like, optional
+        Array with model name strings
+    long_format : bool
+        Boolean flag to interpret data in long or wide format
+
+    Returns
+    -------
+    data : xarray.Dataset
+        Impact dataset containing the impact for each `Item` and
+        `Region` with one dataarray per element in `dataset`.
+    """
+
+    # if the input has a single element, proceed with long format
+    if np.isscalar(quantities):
+        long_format = True
+
+    quantities = np.array(quantities)
+
+    # Identify unique values in coordinates
     _items = np.unique(items)
+    _regions = np.unique(regions)
 
-    size = (len(_impacts), len(_items))
+    coords = {"Item" : _items,
+              "Region" : _regions}
 
-    ii = [np.searchsorted(_impacts, impacts), np.searchsorted(_items, items)]
+    # find positions in output array to organize data
+    ii = [np.searchsorted(_items, items), np.searchsorted(_regions, regions)]
+    size = (len(_items), len(_regions))
 
-    coords = {"Year" : _impacts,
-              "Item" : _items}
+    # Create empty dataset
+    data = xr.Dataset(coords = coords)
 
-    if regions is not None:
-        _regions = np.unique(regions)
-        ii.append(np.searchsorted(_regions, regions))
-        size = size + (len(_regions),)
-        coords["Region"] = _regions
+    if long_format:
+        ndims = 2
+    else:
+        ndims = 3
 
-    values = np.zeros(size)*np.nan
+    # make sure the long format has two dimensions
+    # One along items and regions, one along datasets
+    while len(quantities.shape) < ndims:
+        quantities = np.expand_dims(quantities, axis=0)
 
-    values[tuple(ii)] = quantities
+    # If no dataset names are given, then create generic ones, one for each
+    # dataset
+    if datasets is None:
+        datasets = [f"Impact {id}" for id in range(quantities.shape[0])]
 
-    data = xr.Dataset(data_vars = dict(value=(coords.keys(), values)), coords = coords)
+    # Else, if a single string is given, transform to list. If doesn't match
+    # the number of datasets created above, this will spit a list later.
+    elif isinstance(datasets, str):
+        datasets = [datasets]
+
+    if long_format:
+        # Create a datasets, one at a time
+        for id, dataset in enumerate(datasets):
+            values = np.zeros(size)*np.nan
+            values[tuple(ii)] = quantities[id]
+            data[dataset] = (coords, values)
+
+    else:
+        quantities = quantities[:, ii[0]]
+        quantities = quantities[..., ii[1]]
+
+        # Create a datasets, one at a time
+        for id, dataset in enumerate(datasets):
+            values = quantities[id]
+            data[dataset] = (coords, values)
 
     return data
 
