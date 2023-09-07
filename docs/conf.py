5e6d637e05f0980c470b29e57d37589a502ed307
jucordero <obejaamarilla@hotmail.com>
2023-09-07 12:52:21 +0100

    added napoleon to parse numpy style docstrings

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ release = '0.0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['myst_nb', 'autoapi.extension']
+extensions = ['myst_nb', 'autoapi.extension', 'sphinx.ext.napoleon']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
