184eda0b4fa616a8a4dc51a68ef6f15bc03a6d29
Juan Pablo Cordero <34517350+jucordero@users.noreply.github.com>
2023-11-10 18:27:17 +0000

    Fixes and tests for sequestration model (#48)

--- agrifoodpy/land/model.py
+++ agrifoodpy/land/model.py
@@ -2,131 +2,89 @@
 
 """
 
-import xarray as xr
 import numpy as np
 from agrifoodpy.land.land import LandDataArray
-from agrifoodpy.utils import scaling
 
-def carbon_sequestration(land_da, categories,
-                         f_forest=0,
-                         f_agroforestry=0,
-                         f_silvopasture=0,
-                         f_broadleaf=0.5,
-                         max_seq_broadleaf=5.7,
-                         max_seq_coniferous=14,
-                         max_seq_agroforestry=1.473,
-                         max_seq_silvopasture=6.26,
-                         years=None,
-                         growth_timescale=15):
-    """Additional sequestration model.
+def land_sequestration(land_da, use_id, fraction, max_seq, years=None,
+                       growth_timescale=10, growth="linear", ha_per_pixel=1):
+    
+    """Additional land use sequestration model.
     
     Computes the anual additional sequestration from land use change as a
-    function of the fraction of arable and pasture land to be converted into
-    forest, agroforestry and silvopasture land. 
+    function of the different land category converted fractional areas.
     
-    Given a map with either percentages per pixel or pixel absolute values for
-    arable and pasture land it computes the yearly additional sequestration
-    from the land use change.
+    Given a Land Data Array map with pixel id values for the different land use
+    types, the model computes additional sequestration from land given the new
+    value in [t CO2e / yr].
 
     Parameters
     ----------
-    land : xarray.Dataset or xarray.Dataarray
-        Input land array containing arable and pasture land values per pixel.
-        If a dataarray, the values of the array should contain the index of the
-        dominant category at each position. If a dataset, each dataarray should
-        contain the percentage of each position corresponding to category.
-    categories : int, array
-        Identifiers of the land categories to be converted. The agroforestry,
-        forest and silvopasture array lenghts must match the lenght of this
-        array.
-    forest_fraction : float, array
-        Fraction of each land category converted to forest 
-    agroforestry_fraction : float, array
-        Fraction of each land category converted to agroforestry 
-    silvopasture_fraction : float, array
-        Fraction of each land category converted to silvopasture 
-    broadleaf_ratio : float
-        Relative fraction of broadleaf species used for forestation. Remaining
-        percentage is assumed to be coniferous
-    max_seq_broadleaf : float
-        Fully grown broadleaf forest carbon sequestration expressed in
-        [t CO2e / ha / year]
-    max_seq_coniferous : float
-        Fully grown coniferous forest carbon sequestration expressed in
-        [t CO2e / ha / year]
-    max_seq_agroforestry : float
-        Fully grown agroforestry land carbon sequestration expressed in
-        [t CO2e / ha / year]
-    max_seq_silvopasture : float
-        Fully grown silvopasture land carbon sequestration expressed in
-        [t CO2e / ha / year]    
+    land : xarray.Dataarray
+        Input land array containing the id id value with the dominant land use
+        type on that pixel.
+    use_id : int, array
+        Land category identifiers for the land uses to be converted.
+    fraction : float, array
+        Fraction of each repurposed land category  
+    max_seq : float
+        Maximum sequestration achieved at the end of the growth period in
+        [t CO2e / yr]
+    growth : str
+        Type of sequestration growth. Can be one of "linear" and "logistic"
+    growth_timescale : float
+        Time in years for land to reach full sequestration potential max_seq
     years : int, array
         Year range length, or array of years for which the sequestration is
-        computed. If not given, stationary maximum values are returned 
-    growth_timescale : float
-        Time in years for land to reach full sequestration potential
-
+        computed. If not set, stationary maximum values are returned
+    ha_per_pixel : float
+        Area per pixel in hectares.
 
     Returns
     -------
-    seq : xarray.Dataset
-        Dataset with the per year sequestration from each component
+    seq : xarray.DataArray
+        DataArray with the per year sequestration
     """
     
-    f_forest = np.array(f_forest)
-    f_agroforestry = np.array(f_agroforestry)
-    f_silvopasture = np.array(f_silvopasture)
-    
-    # Check all fractions are between 0 and 1     
-    for arr in [f_forest, f_agroforestry, f_silvopasture]:
-        if not (0 <= arr).all() and (arr <= 1).all():
-            raise ValueError("Input fraction values must be between 0 and 1")
+    if np.isscalar(use_id):
+        use_id = np.array(use_id)
+
+    # If single scalar value, use the same for all categories
+    if np.isscalar(fraction):
+        fraction = np.ones_like(use_id)*fraction
+    else:
+        fraction = np.array(fraction)
+
+    if not (fraction >= 0).all() and (fraction <= 1).all():
+        raise ValueError("Input fraction values must be between 0 and 1")
             
-    # Check all fractions add up to less than 1
-    for i in range(len(categories)):
-        if f_forest[i] + f_agroforestry[i] + f_silvopasture[i] > 1:
-            raise ValueError("Total fraction change for each category must be \
-                             less or equal than 1")
+    pixel_count_category = land_da.land.area_by_type(values=use_id)
     
-    pixel_count_category = land_da.land.area_by_type(values=categories)
     # area in hectares
-    area_category = pixel_count_category * 100
+    area_category = pixel_count_category * ha_per_pixel
+
+    if growth == "linear":
+        from agrifoodpy.utils.scaling import linear_scale as growth_shape
+    elif growth == "logistic":
+        from agrifoodpy.utils.scaling import logistic_scale as growth_shape
+    else:
+        raise ValueError("Growth must be one of 'linear' or 'logistic'")
     
     if years is not None:
         # single scalar value
         if np.isscalar(years):
-            scale = scaling.linear_scale(0, 0, growth_timescale, years, 0, 1)
+            scale = growth_shape(0, 0, growth_timescale, years, 0, 1)
         # year range
         else:
             y0 = np.max((0, np.min(years)))
             y1 = np.max((0, np.min(years)))
             y2 = growth_timescale + y1
             y3 = np.max(years)
-            scale = scaling.linear_scale(y0, y1, y2, y3, 0, 1)
+            scale = growth_shape(y0, y1, y2, y3, 0, 1)
     else:
         scale = 1
-    
-    # afforestation
-    seq_forest = max_seq_broadleaf * (f_broadleaf) + \
-                     max_seq_coniferous * (1-f_broadleaf)
-    
-    forest_area = area_category * f_forest
-    total_seq_forest = forest_area.sum() * scale * seq_forest
-    
+
     # agroforestry    
-    agroforestry_area = area_category * f_agroforestry
-    total_seq_agroforestry = agroforestry_area.sum() * scale * \
-        max_seq_agroforestry
-        
-    # silvopasture    
-    silvopasture_area = area_category * f_silvopasture
-    total_seq_silvopasture = silvopasture_area.sum() * scale * \
-        max_seq_silvopasture
-    
-    sequestration_dataset = xr.Dataset({
-        "afforestation" : total_seq_forest,
-        "agroforestry" : total_seq_agroforestry,
-        "silvopasture" : total_seq_silvopasture
-    })
-    
-    return sequestration_dataset
+    area = area_category * fraction
+    total_seq = area.sum() * scale * max_seq
+
+    return total_seq
--- agrifoodpy/land/tests/test_model.py
+++ agrifoodpy/land/tests/test_model.py
@@ -1,6 +1,50 @@
 import numpy as np
 import xarray as xr
+from agrifoodpy.land.model import land_sequestration
 
-def test_carbon_sequestration():
-    pass
+def test_land_sequestration():
 
+    # basic test with one category and one fraction
+    lda = xr.DataArray([[0,1],[1,0]], coords={"x":[0,1], "y":[0,1]},
+                       name = "land")
+    
+    result = land_sequestration(lda, 0, 0.5, 10)
+    truth = xr.DataArray(data=10)
+
+    xr.testing.assert_equal(result, truth)
+
+    # test with multiple categories and one fraction
+    result = land_sequestration(lda, [0,1], 0.5, 10)
+    truth = xr.DataArray(data=20)
+
+    xr.testing.assert_equal(result, truth)
+
+    # test with multiple categories and multiple fractions
+    result = land_sequestration(lda, [0,1], [0.5, 0.1], 10)
+    truth = xr.DataArray(data=12)
+
+    xr.testing.assert_equal(result, truth)
+
+    # test with years value
+    result = land_sequestration(lda, 0, 0.5, 10, years=10, growth_timescale=10)
+    truth = xr.DataArray(data=np.arange(11), coords={"Year":np.arange(11)})
+
+    xr.testing.assert_allclose(result, truth)
+
+    # test with years array
+    result = land_sequestration(lda, 0, 0.5, 10, years=np.arange(1960,2020),
+                                growth_timescale=10)
+    truth_data = np.concatenate((np.arange(11), np.ones(2020-1960-11)*10))
+    truth = xr.DataArray(data=truth_data, coords={"Year":np.arange(1960, 2020)})
+
+    xr.testing.assert_allclose(result, truth)
+
+    # test with logistic growth function
+    result = land_sequestration(lda, 0, 0.5, 10, years=10, growth_timescale=10,
+                                growth="logistic")
+    truth = xr.DataArray([0.06692851, 0.1798621 , 0.47425873, 1.19202922,
+                        2.68941421, 5.        , 7.31058579, 8.80797078,
+                        9.52574127, 9.8201379 , 10.],
+                        coords={"Year":np.arange(11)})
+
+    xr.testing.assert_allclose(result, truth)
