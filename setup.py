d66a89b80952453c36c9bdae7f8b622fb84770e9
jucordero <obejaamarilla@hotmail.com>
2023-11-17 13:08:07 +0000

    Fixed setup version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = '0.1.0'
+VERSION = '0.1.1'
 PACKAGE_NAME = 'AgriFoodPy'
 AUTHOR = 'FixOurFood developers'
 AUTHOR_EMAIL = 'juanpablo.cordero@york.ac.uk'
