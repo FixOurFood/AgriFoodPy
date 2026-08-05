9d0b926ea1d12203c2f85f6789781a99d47990ad
jucordero <obejaamarilla@hotmail.com>
2026-08-05 13:50:17 +0100

    bump version to 0.2.1

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = 'agrifoodpy'
 copyright = '2023, AgriFoodPy developers'
 author = 'Juan P. Cordero'
-release = '0.2.0'
+release = '0.2.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = '0.2.0'
+VERSION = '0.2.1'
 PACKAGE_NAME = 'AgriFoodPy'
 AUTHOR = 'FixOurFood developers'
 AUTHOR_EMAIL = 'juanpablo.cordero@york.ac.uk'
